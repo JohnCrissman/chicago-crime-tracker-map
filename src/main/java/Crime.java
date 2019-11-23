3b46ce5938c341d10f1c32c531519d95811b722e
Marianela Crissman <marianela_min@yahoo.com>
2019-11-22 21:49:36 -0600

    added Address type as an instance variable. Also added comments

--- src/main/java/Crime.java
+++ src/main/java/Crime.java
@@ -4,28 +4,31 @@ import java.util.Date;
 public class Crime {
     private String type; // primary_type
     private String typeDescription; // secondary_type
-    private Address address;
+    private Address address;  // address contains block street, lat and lon
     private Date date;
 
-
+    // default constructor
     public Crime ()
     {
         this.address = new Address();
     }
 
-
+    // constructor given almost all primitive types -- for testing
     public Crime(String type, Double latitude, Double longitude, Date date, String block){
         this.type = type;
         this.address = new Address(latitude, longitude, block);
         this.date = date;
     }
 
+    // constructor given an address object
     public Crime(String type, Date date, Address address){
         this.type = type;
         this.date = date;
         this.address = address;
     }
 
+
+    // getters
     public String getType(){
         return this.type;
     }
@@ -36,7 +39,6 @@ public class Crime {
         return this.address.getLongitude();
     }
     public Double getLatitude() { return this.address.getLatitude(); }
-
     public Date getDate(){
         return this.date;
     }
