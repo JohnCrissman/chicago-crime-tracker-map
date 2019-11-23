99901920a8b2e3406a59d654b951d6f1ffab4bb9
Marianela Crissman <marianela_min@yahoo.com>
2019-11-22 20:21:37 -0600

    added method that greates a new list with the creimes within the filter

--- src/main/java/m_CrimeList.java
+++ src/main/java/m_CrimeList.java
@@ -1,26 +1,34 @@
 import java.util.LinkedList;
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 public class m_CrimeList {
-    private double radius;
-    private List<j_Crime> crimes;
+    private List<Crime> crimes; //list of loaded crimes for the past 2 weeks
+    private List<CrimeRelativeToAddress> crimesRelativeTo;
 
-    public m_CrimeList(double radius, List<j_Crime> crimes){
-        this.radius = radius;
-        this.crimes = crimes;
+    public m_CrimeList(){
+
     }
 
-    public double getRadius(){
-        return this.radius;
-    }
-
-    public List<j_Crime> getCrimes(){
+    public List<Crime> getCrimes(){
         return this.crimes;
     }
 
+    public List<CrimeRelativeToAddress> crimesWithinRadius(double radius, Address address){
+        // TODO: make conversion of the radius from miles to the corresponding unit that lat/lon use.
+        return this.crimes.stream()
+                .map(crime -> new CrimeRelativeToAddress(crime))
+                .peek(cRel -> cRel.setProximity(address))
+                .filter(cRel -> Distance.LatLong.isWithinRadius(cRel.getAddress(), address, radius))
+                .collect(toList());
+    }
 
     public List<Crime> query() { // not sure what the parameter should be.
-        // make a list of crimes
-        return new LinkedList<Crime>();
+        // add crimes to th list
+        List<Crime> theList = new LinkedList<>();
+
+
+        return theList;
     }
 }
