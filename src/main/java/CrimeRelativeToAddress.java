164aabcf34d5decd411eb73008dcf505fc7e3f29
Marianela Crissman <marianela_min@yahoo.com>
2019-11-22 19:57:01 -0600

    cleaned code

--- src/main/java/CrimeRelativeToAddress.java
+++ src/main/java/CrimeRelativeToAddress.java
@@ -15,13 +15,7 @@ public class CrimeRelativeToAddress extends Crime {
     }
 
     public void setProximity(Address address) {
-        // converts the address (parameter) to a list of two elements (latitude and longitude)
-//        System.out.println("Address: " + address.longitude + " " + address.latitude);
-//        System.out.println("long: " + this.getLongitude() +" "+ this.getLongitude().getClass());
-//        System.out.println("lat: " + this.getLatitude() + " " +this.getLatitude().getClass());
         this.proximity = Distance.LatLong.distanceBetweenTwoLocations(this.getAddress(), address);
-//        System.out.println("proximity would be: " + this.proximity);
-
     }
 
     @Override
