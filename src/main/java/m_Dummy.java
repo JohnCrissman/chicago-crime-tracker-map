411210e7ed0cf99c2231c1ee8ec4b4e9a0275282
Marianela Crissman <marianela_min@yahoo.com>
2019-12-01 19:00:42 -0600

    created a java class to help the communication between Java and JavaScript. Added a function in JS that plots the markers and a nice infoWindow. Also centers the search based on what was requested.

---
+++ src/main/java/m_Dummy.java
@@ -0,0 +1,65 @@
+import javafx.scene.control.Button;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import org.json.simple.JSONArray;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class m_Dummy {
+
+
+    public static Button createDummyBtn(WebView mapView, Crimes latestCrimes){
+        //TODO: ERASE <--- dummybutton for testing
+        Button dummyButton = new Button("Dummy Btn");
+        //TODO: END ERASE<---
+
+        //TODO: ERASE <--- dummybutton set on action for testing
+        dummyButton.setPrefSize(100, 20);
+        dummyButton.setOnAction(e ->{
+            execJsMethod(mapView, latestCrimes, "all");
+        });
+        //TODO: END ERASE <---
+
+        return dummyButton;
+    }
+
+    private static void execJsMethod(WebView mapView, Crimes latestCrimes, String jsFuncName) {
+        String jsFunctionCall;
+        if (jsFuncName.equalsIgnoreCase("all")){
+            jsFunctionCall = "showCrimesOnMap('" +
+                JSONArray.toJSONString(latestCrimes.getAllCrimes()
+                        .stream()
+//                                        .map(crime -> crime.getAddress())
+//                        .limit(5)
+                        .collect(toList())) + "', '" + latestCrimes.getRelativeAddress().toString() + "')";
+
+        }else if(jsFuncName.equalsIgnoreCase("rel")){
+            jsFunctionCall = "showCrimesOnMap('" +
+                    JSONArray.toJSONString(latestCrimes.getCrimesRelativeTo()
+                            .stream()
+                            .sorted((cp1,cp2) -> (int)(cp1.getProximity() - cp2.getProximity()))
+//                             .map(crime -> crime.getAddress())
+                            .limit(10)
+                            .collect(toList())) + "', '" + latestCrimes.getRelativeAddress().toString() + "')";
+
+        }else{
+            jsFunctionCall = "showCrimesOnMap('" +
+                    JSONArray.toJSONString(latestCrimes.getAllCrimes()
+                            .stream()
+//                             .map(crime -> crime.getAddress())
+                            .limit(5)
+                            .collect(toList())) + "', '" + latestCrimes.getRelativeAddress().toString() + "')";
+        }
+
+        System.out.println(jsFunctionCall);
+        mapView.getEngine().executeScript(jsFunctionCall);
+    }
+
+
+
+    public static void execJsFunc(WebView webView, Crimes latestCrimes, String jsFuncName){
+        execJsMethod(webView, latestCrimes, jsFuncName);
+    }
+}
--- src/main/resources/b_mapdemo.js
+++ src/main/resources/b_mapdemo.js
@@ -1,7 +1,7 @@
 function initialize() {
                 var myOptions = {
-                      zoom: 14,
-                      center: new google.maps.LatLng(41.95, -87.7),
+                      zoom: 13,
+                      center: new google.maps.LatLng(41.9803467, -87.7191019),
                       mapTypeId: google.maps.MapTypeId.ROADMAP,
                       mapTypeControl: false,
                       navigationControl: false,
@@ -9,4 +9,56 @@ function initialize() {
                       backgroundColor: "#666970"
                     };
                 var map = new google.maps.Map(document.getElementById("googleMap"),myOptions);
+
+                /*var latLng = new google.maps.LatLng(41.95, -87.7);
+                var marker = new google.maps.Marker({
+                    position: latLng,
+                    map: map
+                });*/
+
            }
+
+function showCrimesOnMap(cr, centerAddress){
+        let domElement = document.querySelector('#dummyBtn');
+        domElement.innerHTML = `<h1>Hi Beth!</h1><div><small>I am a div that will be eliminated after all testing is done!</small></div> <br/><pre styles="width:100%;"> ${cr} </pre> <br/><ol>`;
+        
+        const crimes = JSON.parse(cr);
+        const addre = JSON.parse(centerAddress);
+        domElement.innerHTML+= `<h1>${addre.lat} ${addre.long}</h1>`;
+        let mapOptions = {
+            zoom: 13,
+            center: new google.maps.LatLng(addre.lat, addre.long), //41.95, -87.7),
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+          };
+        map = new google.maps.Map(document.getElementById("googleMap"),mapOptions);
+        
+        crimes.forEach((c,i) => {
+            let crimeInfo = crimeInfoWindow(c,i);
+            let marker = new google.maps.Marker({
+                position: new google.maps.LatLng(c.address.lat, c.address.long),
+                label: (i+1)+"",
+                zIndex: (-1)*(i+1),
+                map: map
+            });
+            let infoWindow = new google.maps.InfoWindow({
+                content: crimeInfo
+            });
+            marker.addListener('click', (e) => {
+                infoWindow.open(map, marker);
+            });
+            
+            domElement.innerHTML += crimeInfo;
+        });
+        domElement.innerHTML += '</ol>';
+    }
+
+function crimeInfoWindow(c, i){
+    return `<span> (${i+1}) </span>
+    <h2>${c.type}</h2>
+    <h3>${c.typeDescription}</h3>
+    <ul>
+        <li>Address: ${c.address.block + c.address.street} </li>
+        <li>LatLong: (${c.address.lat}, ${c.address.long}) </li>
+        <li>Date: ${c.date} </li>
+    </ul>`;//`<h2>test here ${i+1}</h2> <ol>` + crimeInfo + `</ol>`
+}
