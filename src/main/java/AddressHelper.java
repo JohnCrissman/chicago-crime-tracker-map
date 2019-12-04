c074d39a8c12a4bbb30a771db5b4c1b285913eac
emcdona1 <emcdona1@neiu.edu>
2019-12-04 10:19:25 -0600

    Best practices for utility class (private constructor, final class)

--- src/main/java/AddressHelper.java
+++ src/main/java/AddressHelper.java
@@ -4,43 +4,42 @@ import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
-public class AddressHelper {
+public final class AddressHelper {
+    private AddressHelper() {}
 
+    public static double distanceBetweenTwoLocations(Address add1, Address add2){
+        double xDelta = add1.getLongInMiles() - add2.getLongInMiles();
+        double yDelta = add1.getLatInMiles() - add2.getLatInMiles();
+        return Math.sqrt(Math.pow(xDelta, 2) + Math.pow(yDelta, 2));
+    }
 
-        public static double distanceBetweenTwoLocations(Address add1, Address add2){
-            double xDelta = add1.getLongInMiles() - add2.getLongInMiles();
-            double yDelta = add1.getLatInMiles() - add2.getLatInMiles();
-            return Math.sqrt(Math.pow(xDelta, 2) + Math.pow(yDelta, 2));
+    public static boolean isWithinRadius(Address add1, Address add2, Double radius){
+        // TODO: Troubleshoot this!
+        //  "2400 N St Louis Ave"/"2 mi" returns 604 crimes,
+        //  "2900 N St Louis Ave"/"5 mi" returns 0 crimes.
+        double distance = distanceBetweenTwoLocations(add1, add2);
+        boolean result = distance < radius;
+        return result;
+    }
+
+    public static Address getAddressFromGoogleAPI(String address) throws  NotAnAddressException, IOException{
+        String googleApiKey = "AIzaSyCN7hTS17iGOG-yLy7lBknC5TcCUCHq7Qo";
+        String url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + URLEncoder.encode(address, StandardCharsets.UTF_8) + "&key=" + googleApiKey;
+
+        JSONObject jobj = APITalker.getObjectResponse(url, false);
+
+        try{
+            JSONArray jar = (JSONArray) jobj.get("results");
+            JSONObject job = (JSONObject) jar.get(0);
+            String block = (String) job.get("formatted_address");
+            job = (JSONObject) job.get("geometry");
+            job = (JSONObject) job.get("location");
+            double longitud = (double) job.get("lng");
+            double latitud = (double) job.get("lat");
+            return new Address(latitud,longitud,block);
         }
-
-        public static boolean isWithinRadius(Address add1, Address add2, Double radius){
-            // TODO: Troubleshoot this!
-            //  "2400 N St Louis Ave"/"2 mi" returns 604 crimes,
-            //  "2900 N St Louis Ave"/"5 mi" returns 0 crimes.
-            double distance = distanceBetweenTwoLocations(add1, add2);
-            boolean result = distance < radius;
-            return result;
+        catch (Exception e){
+            throw new NotAnAddressException("Not valid address:" + address);
         }
-
-        public static Address getAddressFromGoogleAPI(String address) throws  NotAnAddressException, IOException{
-            String googleApiKey = "AIzaSyCN7hTS17iGOG-yLy7lBknC5TcCUCHq7Qo";
-            String url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + URLEncoder.encode(address, StandardCharsets.UTF_8) + "&key=" + googleApiKey;
-
-            JSONObject jobj = APITalker.getObjectResponse(url, false);
-
-            try{
-                JSONArray jar = (JSONArray) jobj.get("results");
-                JSONObject job = (JSONObject) jar.get(0);
-                String block = (String) job.get("formatted_address");
-                job = (JSONObject) job.get("geometry");
-                job = (JSONObject) job.get("location");
-                double longitud = (double) job.get("lng");
-                double latitud = (double) job.get("lat");
-                return new Address(latitud,longitud,block);
-            }
-            catch (Exception e){
-                throw new NotAnAddressException("Not valid address:" + address);
-            }
-        }
-
+    }
 }
